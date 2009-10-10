2047c54852fc45131f851fb75b15ad209c5c6cb7
Assaf Arkin <assaf@labnotes.org>
2009-10-09 18:02:14 -0700

    Added read, write methods, changed fix_file to patch. Now processing some tasks all the time, and ordered/stamped tasks only once.

--- lib/necktie/files.rb
+++ lib/necktie/files.rb
@@ -1,8 +1,24 @@
-def fix_file(file, &block)
+def patch(file, &block)
   text = block.call(File.read(file))
   File.open file, "w" do |out|
     out.write text
   end
+end
+
+def write(name, content = "")
+  File.open name, "w" do |f|
+    f.write content
+  end
+end
+
+def append(name, content = "")
+  File.open name, "a" do |f|
+    f.write content
+  end
+end
+
+def read(name)
+  File.read(name)
 end
 
 def touch(name)
