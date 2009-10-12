54bcc86541d8ac4bc613b0a40b5563aa8eb7525f
Assaf Arkin <assaf@labnotes.org>
2009-10-12 15:51:18 -0700

    Yay for documentation!

--- extra/capistrano/necktie.rb
+++ extra/capistrano/necktie.rb
@@ -1,7 +1,6 @@
-set :necktie_git, "git@example.com:striped"
-set :env, "staging"
+set :necktie_url, "git@example.com:striped"
 
-task :setup, :roles=>:app do
+task :necktie do
   # Copy Gem over so not depending on gem server being online, and all servers get
   # to use the same version of Necktie.
   gem_spec = Gem::SourceIndex.from_installed_gems.find_name("necktie").last
@@ -9,6 +8,6 @@ task :setup, :roles=>:app do
   upload gem_file, File.basename(gem_file), :via=>:scp
   sudo "gem install #{File.basename(gem_file)}"
   # Run Necktie as sudo.
-  sudo "necktie #{necktie_git} app env=#{env}"
+  sudo "necktie #{necktie_url} #{ENV["ROLES"].gsub(',', ' ')} RAILS_ENV=#{fetch(:rails_env, "production")}"
 end
-before "deploy:cold", "setup"
+before "deploy:cold", "necktie"
